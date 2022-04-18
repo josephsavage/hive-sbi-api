5494f597e0d8afa91de0ba2a7785fa3d7bdf6c33
Andrés Quiroga <andres.quiroga6041@gmail.com>
2022-04-18 10:50:22 -0500

    Transfor uper case letter for db querying

--- app/hive_sbi_api/v0/views.py
+++ app/hive_sbi_api/v0/views.py
@@ -5,6 +5,7 @@ from datetime import (datetime,
 from rest_framework.mixins import (ListModelMixin,
                                    RetrieveModelMixin)
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from drf_yasg import openapi
@@ -54,6 +55,34 @@ class MemberViewSet(RetrieveModelMixin,
     serializer_class = MemberSerializer
 
     user_response = openapi.Response('response description', UserSerializer)
+
+    def get_object(self):
+        """
+        Returns the object the view is displaying.
+        You may want to override this if you need to provide non-standard
+        queryset lookups.  Eg if objects are referenced using multiple
+        keyword arguments in the url conf.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg].lower()}
+
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
     @swagger_auto_schema(tags=['V0'], responses={200: user_response})
     def retrieve(self, request, *args, **kwargs):
--- app/hive_sbi_api/v1/views.py
+++ app/hive_sbi_api/v1/views.py
@@ -5,6 +5,7 @@ from rest_framework.mixins import (ListModelMixin,
 
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.filters import OrderingFilter
+from rest_framework.generics import get_object_or_404
 
 from hive_sbi_api.core.models import Member
 from .serializers import MemberSerializer
@@ -24,3 +25,31 @@ class MemberViewSet(ListModelMixin,
 
     filter_backends = [OrderingFilter]
     ordering_fields = ['shares', 'bonus_shares', 'estimate_rewarded', 'total_rshares']
+
+    def get_object(self):
+        """
+        Returns the object the view is displaying.
+        You may want to override this if you need to provide non-standard
+        queryset lookups.  Eg if objects are referenced using multiple
+        keyword arguments in the url conf.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg].lower()}
+
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
