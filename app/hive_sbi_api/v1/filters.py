f87af53f35174f215bbe17f4562db6084b1f7a88
Andrés Quiroga <andres.quiroga6041@gmail.com>
2022-06-04 01:36:12 -0500

    Better code

--- app/hive_sbi_api/v1/filters.py
+++ app/hive_sbi_api/v1/filters.py
@@ -4,6 +4,16 @@ from hive_sbi_api.core.models import Transaction
 
 
 class TransactionFilter(filters.FilterSet):
+    account = filters.CharFilter(
+        field_name='account__account',
+        label='account',
+    )
+
+    sponsor = filters.CharFilter(
+        field_name='sponsor__account',
+        label='sponsor',
+    )
+
     sponsee = filters.CharFilter(
         field_name='sponsees__account__account',
         label='sponsee',
