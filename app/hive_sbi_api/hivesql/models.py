31283b4831fffb6178d4e1b8c8f48b46e9104965
Andrés Quiroga <andres.quiroga6041@gmail.com>
2022-11-03 09:32:37 -0500

    Added comments table

--- app/hive_sbi_api/hivesql/models.py
+++ app/hive_sbi_api/hivesql/models.py
@@ -7,6 +7,26 @@ class HiveSQLManager(models.Manager):
         self._db = 'hivesql'
 
 
+class HiveSQLComment(models.Model):
+    objects = HiveSQLManager()
+
+    author = models.CharField(max_length=50)
+    permlink = models.CharField(max_length=512)
+    title = models.TextField()
+    created = models.DateTimeField()
+    vote_rshares = models.BigIntegerField()
+    total_payout_value = models.FloatField()
+    author_rewards = models.FloatField()
+    active_votes = models.JSONField()
+
+    def __str__(self):
+        return "{} - {}".format(self.permlink, self.author)
+
+    class Meta:
+        managed = False
+        db_table = 'Comments'
+
+
 class HiveSQLTxVotes(models.Model):
     objects = HiveSQLManager()
 
