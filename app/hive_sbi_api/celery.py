324bbf08ae492ef5d5f92eacc3da878f8f7ef470
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2026-02-18 19:12:10 -0800

    Update celery.py

--- app/hive_sbi_api/celery.py
+++ app/hive_sbi_api/celery.py
@@ -1,18 +1,16 @@
-from celery import Celery
-from django.conf import settings
-
 import os
+import django
+from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hive_sbi_api.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hive_sbi_api.settings")
 
-app = Celery('service', backend=settings.CELERY_BROKER_URL)
+# Initialize Django BEFORE creating the Celery app or autodiscovering tasks
+django.setup()
 
-# Using a string here means the worker don't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery("service")
 
-# Load task modules from all registered Django app configs.
+# Load Celery config from Django settings
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Autodiscover tasks in installed apps
 app.autodiscover_tasks()
